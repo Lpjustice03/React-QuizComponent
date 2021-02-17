eac7f0517fb2a4658a5f6cd04646162dacd4ffba
Lpjustice03 <64537647+Lpjustice03@users.noreply.github.com>
2021-02-17 17:07:31 -0500

    Completed module

--- src/quiz.js
+++ src/quiz.js
@@ -9,7 +9,7 @@ class Quiz extends Component{
     render(){
         return(
         <div className="QuizQuestion">
-          {quizData.quiz_questions[0].instruction_text}
+         <h5> {quizData.quiz_questions[0].instruction_text}</h5>
             </div>
         );
     }
